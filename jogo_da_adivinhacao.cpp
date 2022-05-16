00eca07a7c59a06c530fc7228e7f03741265d7ce
João Macêdo <99701121+joaomacedx@users.noreply.github.com>
2022-05-16 19:04:25 -0300

    Adicionando tentativas ao jogo

--- jogo_da_adivinhacao.cpp
+++ jogo_da_adivinhacao.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <time.h>
 using namespace std;
 
 int main (){
@@ -9,21 +9,50 @@ int main (){
 		 
 		 
      int numerosecreto = 42;
-     int chute;
-         cout << "Chute um numero de 1 a 100" << endl;
-         cin >> chute;
-         cout << "O valor do seu chute e: " << chute << endl;
+     int tentativas = 0;
+     // int chute;
+         //cout << "Chute um numero de 1 a 100" << endl;
+         //cin >> chute;
+         //cout << "O valor do seu chute e: " << chute << endl;
          
-         bool acertou = chute == numerosecreto;
-         bool menor = chute < numerosecreto;
-         if (acertou){
+         //bool acertou = chute == numerosecreto;
+         //bool menor = chute < numerosecreto;
+         // if (acertou){
+         // 	cout << "Parabens! Voce acertou o numero secreto" << endl;
+		 // }
+		 // else if(menor){
+		 // 	cout << "Voce errou, o seu chute foi menor que o numero secreto!" << endl;
+		 // }
+		 //else{
+		 //	cout << "Voce errou, o seu chute foi maior que o numero secreto!" << endl;
+		 //}
+		 bool naoAcertou = true; 
+		 while (naoAcertou)
+		 {
+		 	 tentativas++;
+		 	 int chute;
+		 	 cout << "Tentativa " << tentativas << endl;
+		 	 cout << "Chute um numero de 1 a 100" << endl;
+             cin >> chute;
+             cout << "O valor do seu chute e: " << chute << endl;
+             bool acertou = chute == numerosecreto;
+             bool menor = chute < numerosecreto;
+		 	 if (acertou)
+			  {
          	cout << "Parabens! Voce acertou o numero secreto" << endl;
-		 }
-		 else if(menor){
+         	naoAcertou = false;
+         	//break;
+		 	}
+			 else if(menor)
+		 	{
 		 	cout << "Voce errou, o seu chute foi menor que o numero secreto!" << endl;
-		 }
-		 else{
+		 	}
+		 	else
+		 	{
 		 	cout << "Voce errou, o seu chute foi maior que o numero secreto!" << endl;
+		 	}
 		 }
      
+     cout << "FIM DE JOGO!!" <<endl;
+     cout << "Voce acertou em " << tentativas <<  " tentativas" <<endl;
 }
