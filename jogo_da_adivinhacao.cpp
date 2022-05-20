a71f4a4aa3f6b256752a2066c4edda79c8ee3403
João Macêdo <99701121+joaomacedx@users.noreply.github.com>
2022-05-20 16:48:54 -0300

    Correção na acentuação dos caracteres do jogo

--- jogo_da_adivinhacao.cpp
+++ jogo_da_adivinhacao.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <time.h>
 #include <cmath>
+#include <locale.h>
 using namespace std;
 
 int main (){
+	     setlocale (LC_ALL, "portuguese");
 		 cout<< "************************************" << endl;
-		 cout<< "*Bem-vindos ao jogo de adivinhacao!*" << endl;
+		 cout<< "*Bem-vindos ao jogo de adivinhação!*" << endl;
 		 cout<< "************************************" << endl;
 		 
-		 cout << "!VOCE INICIA O JOGO COM 1000 PONTOS!" << endl;
+		 cout << "VOCÊ INICIA O JOGO COM 1000 PONTOS" << endl;
 		 
      int numerosecreto = 42;
      int tentativas = 0;
@@ -21,31 +23,33 @@ int main (){
 		 	 tentativas++;
 		 	 int chute;
 		 	 cout << "Tentativa " << tentativas << endl;
-		 	 cout << "Chute um numero de 1 a 100" << endl;
+		 	 cout << "Chute um número de 1 a 100" << endl;
              cin >> chute;
              
              double pontosPerdidos = abs(chute-numerosecreto)/2.0;
              pontos = pontos - pontosPerdidos;
              
-             cout << "O valor do seu chute e: " << chute << endl;
+             cout << "O valor do seu chute é: " << chute << endl;
              bool acertou = chute == numerosecreto;
              bool menor = chute < numerosecreto;
 		 	 if (acertou)
 			  {
-         	cout << "Parabens! Voce acertou o numero secreto" << endl;
+         	cout << "Parabéns! Você acertou o número secreto" << endl;
          	naoAcertou = false;
 		 	}
 			 else if(menor)
 		 	{
-		 	cout << "Voce errou, o seu chute foi menor que o numero secreto!" << endl;
+		 	cout << "Você errou, o seu chute foi menor que o número secreto!" << endl;
 		 	}
 		 	else
 		 	{
-		 	cout << "Voce errou, o seu chute foi maior que o numero secreto!" << endl;
+		 	cout << "Você errou, o seu chute foi maior que o número secreto!" << endl;
 		 	}
 		 }
      
      cout << "FIM DE JOGO!!" <<endl;
-     cout << "Voce acertou em " << tentativas <<  " tentativas, e sua pontuacao foi " << pontos << " pontos!!"<<endl;
+     cout.precision(2);
+     cout << fixed;
+     cout << "Você acertou em " << tentativas <<  " tentativas, e sua pontuação foi " << pontos << " pontos!!"<<endl;
      
 }
